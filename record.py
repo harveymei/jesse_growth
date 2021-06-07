cafa5968ceff54104c4da29c3f144fe8b6aa1c3c
Harvey Mei <harvey.mei@msn.com>
2021-06-07 17:15:18 +0800

    Initial commit

---
+++ record.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @Time    : 2021/6/7 16:55
+# @Author  : Harvey Mei <harvey.mei@msn.com>
+# @FileName: record.py
+# @IDE     : PyCharm
+# @GitHub  : https://github.com/harveymei/
+
+from matplotlib import pyplot as plt
+from datetime import datetime as dt
+
+"""三坐标轴"""
+
